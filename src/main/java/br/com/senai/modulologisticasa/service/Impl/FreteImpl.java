e52e4835517a18067fce3f72374faa9725320cb3
JOSÉ RUBENS SILVEIRA PEDROSO <jose_r_pedroso@estudante.sesisenai.org.br>
2023-11-09 19:49:40 -0300

    feito frete service

---
+++ src/main/java/br/com/senai/modulologisticasa/service/Impl/FreteImpl.java
@@ -0,0 +1,54 @@
+package br.com.senai.modulologisticasa.service.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.base.Preconditions;
+
+import br.com.senai.modulologisticasa.entity.Frete;
+import br.com.senai.modulologisticasa.repository.FretesRepository;
+import br.com.senai.modulologisticasa.service.FreteService;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Service
+public class FreteImpl implements FreteService {
+
+	@Autowired
+	private FretesRepository repository;
+
+	@Override
+	public Frete salvar(@NotNull(message = "O Frete não pode ser nulo") 
+	Frete frete) {
+		return frete;
+	}
+
+	@Override
+	public void atualizarStatusPor(
+			@Positive(message = "O id para atualização deve ser positivo") 
+			@NotNull(message = "O id é obrigatório") 
+			Integer id,
+			@NotNull(message = "O novo status não pode ser nulo") 
+			Integer status) {
+		
+		Frete freteEncontrado = repository.buscarPorId(id);
+		Preconditions.checkNotNull(freteEncontrado, "Não existe Frete vinculado ao ID informado");
+		Preconditions.checkArgument(freteEncontrado.getStatus() != status,
+				"O status já esta salvo para o Frete");
+		this.repository.atualizarPor(id, status);
+		
+	}
+
+	@Override
+	public Frete buscarPor(
+			@Positive(message = "O id para busca deve ser positivo") 
+			@NotNull(message = "O id é obrigatório") 
+			Integer id) {
+		Frete freteEncontrado = repository.buscarPorId(id);
+		Preconditions.checkNotNull(freteEncontrado, "Não foi encontrado frete para o id informado");
+		return freteEncontrado;
+	}
+	
+	
+	
+}
---
+++ src/main/java/br/com/senai/modulologisticasa/service/proxy/FreteServiceProxy.java
@@ -0,0 +1,47 @@
+package br.com.senai.modulologisticasa.service.proxy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import br.com.senai.modulologisticasa.entity.Frete;
+import br.com.senai.modulologisticasa.service.FreteService;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Service
+public class FreteServiceProxy implements FreteService{
+
+	@Autowired
+	@Qualifier("freteServiceImpl")
+	private FreteService service;
+
+	@Override
+	public Frete salvar(@NotNull(message = "O Frete não pode ser nulo")
+	Frete frete) {
+		
+		return service.salvar(frete);
+	}
+
+	@Override
+	public void atualizarStatusPor(
+			@Positive(message = "O id para atualização deve ser positivo")
+			@NotNull(message = "O id é obrigatório") 
+			Integer id,
+			@NotNull(message = "O novo status não pode ser nulo")
+			Integer status) {
+		this.service.atualizarStatusPor(id, status);
+	}
+
+	@Override
+	public Frete buscarPor(
+			@Positive(message = "O id para busca deve ser positivo")
+			@NotNull(message = "O id é obrigatório") 
+			Integer id) {
+		return service.buscarPor(id);
+	}
+	
+	
+	
+	
+}
