b82c186438522ac5ee6c1dc03cc1fef309780e8e
GUSTAVO DAGOSTIM TONETTO <gustavo_tonetto@estudante.sc.senai.br>
2023-11-09 21:17:45 -0300

    Adicionado dto de localizacao e solicitacao de token

---
+++ src/main/java/br/com/senai/modulologisticasa/dto/Localizacao.java
@@ -0,0 +1,19 @@
+package br.com.senai.modulologisticasa.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Localizacao {
+	
+	@Size(max = 9, message = "O cep de origem não pode conter mais de 9 caracteres")
+	@NotBlank(message = "O cep de origem é obrigatório")
+	private String origem;
+	
+	@Size(max = 9, message = "O cep de origem não pode conter mais de 9 caracteres")
+	@NotBlank(message = "O cep de origem é obrigatório")
+	private String destino;
+}
